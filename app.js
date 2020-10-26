bac0a488566a4bf33c68dcf2ad8245aa0350ac56
zhaofelix <15021179915@163.com>
2020-10-26 10:50:27 +0800

    feat:app.js文件配置
    配置内容：
    - 自动路由接入
    - 日志接入

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//  var logger = require('morgan');
+var logger = require('./utils/logger')
+var cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index')
 
 var app = express();
 
@@ -13,14 +14,49 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+// 配置日志格式
+// 开发环境下的日志格式
+if (process.env.NODE_ENV == 'development') {
+  app.use(
+    logger.logger('dev', {
+      stream: process.stdout
+    })
+  )
+}
+// 生产环境下的日志格式
+else {
+  app.use(logger.logger('combined'))
+}
+
+// 日志写入
+app.use(logger.accessLog)
+app.use(logger.accessLogErr)
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 解决跨域问题
+app.use(cors())
+
+// 路由设置
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+!(function () {
+  var routes = require('./utils/get-routers')
+  var keys = Object.keys(routes) // 获取数组对象中方的key
+  //   循环编列key
+  keys.forEach(function (k) {
+    let cPath = ''
+    if (!!routes[k].cPath) {
+      cPath = routes[k].cPath
+    } else {
+      cPath = k
+    }
+    app.use('/' + cPath, routes[k])
+  })
+})()
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
