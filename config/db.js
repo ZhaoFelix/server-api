7b7e9c8f918a357071f9538b80d0508f5c37e81b
zhaofelix <15021179915@163.com>
2020-10-26 10:59:01 +0800

    feat:连接数据库

---
+++ config/db.js
@@ -0,0 +1,30 @@
+var mysql = require('mysql')
+let config = require('./env')
+//创建一个连接池
+let pool = mysql.createPool(
+  process.env.NODE_ENV == config.prd.env ? config.prd.dbInfo : config.dev.dbInfo
+)
+
+//数据库查询，查询时的默认参数为空
+function queryDB(sql, params = '1', callback) {
+  pool.getConnection(function (err, connection) {
+    if (err) {
+      console.log('连接失败' + err)
+    } else {
+      connection.query(sql, params, function (err, results, fields) {
+        if (err) {
+          console.log('查询失败' + err)
+          // throw err
+        }
+        //查询结束后关闭数据库连接
+        connection.release() //释放连接
+        //将查询出来的结果返回给回调函数
+        callback(err, results, fields)
+      })
+    }
+  })
+}
+
+module.exports = {
+  queryDB
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var DB = require('../config/db')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  DB.queryDB("select * from  t_user_list",function(error,results,fields){
+    if (error){
+      console.log("数据库连接测试失败，Error",error)
+    } else {
+      console.log("数据库连接测试成功。")
+    }
+  })
   res.render('index', { title: 'Express' });
 });
 
