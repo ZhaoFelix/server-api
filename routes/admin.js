8c8b84c2aef73ec99f25311aff7d9a0081babcb3
zhaofelix <15021179915@163.com>
2020-10-27 15:20:15 +0800

    feat:新增管理员接口
    增加接口如下：
    - 管理员查询
    - 管理员新增
    - 管理员登录、登出

---
+++ routes/admin.js
@@ -0,0 +1,128 @@
+/*
+    管理员用户相关的操作
+*/
+
+var express = require('express')
+var router = express.Router()
+var DB = require('../config/db')
+var stringRandom = require('string-random')
+var url = require('url')
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource')
+})
+
+// 查看所有的管理员
+router.get('/query/all', function (req, res, next) {
+  DB.queryDB(
+    'select * from `t_admin_list` where admin_is_deleted = 0',
+    function (error, result, fields) {
+      if (error) {
+        let responseJson = {
+          code: 20002,
+          message: 'error',
+          data: error
+        }
+        res.send(responseJson)
+      } else {
+        let responseJson = {
+          code: 20000,
+          message: 'success',
+          data: result
+        }
+        res.send(responseJson)
+      }
+    }
+  )
+})
+
+//  新增管理员
+router.post('/update/add', function (req, res, next) {
+  let admin_token = stringRandom(16) //生成包含数字和字母的16为字符串
+  let admin_login_name = req.body.editor_login_name
+  let admin_name = req.body.editor_name
+  let admin_pwd = req.body.editor_pwd
+
+  DB.queryDB(
+    'INSERT INTO `t_admin_list` (`admin_login_name`,`admin_name`,`admin_pwd`,`admin_token`,`admin_created_time`) VALUES (?,?,?,?,NOW())',
+    [admin_login_name, admin_name, admin_pwd, admin_token],
+    function (error, result, fields) {
+      if (error) {
+        let responseJson = {
+          code: 20002,
+          message: 'error',
+          data: error
+        }
+        res.send(responseJson)
+      } else {
+        let responseJson = {
+          code: 20000,
+          message: 'success',
+          data: result
+        }
+        res.send(responseJson)
+      }
+    }
+  )
+})
+
+// 删除管理员
+router.post('/update/delete', function (req, res, next) {
+  let id = req.body.id
+  DB.queryDB(
+    'UPDATE `t_admin_list` SET admin_is_deleted = 0  WHERE admin_id = ?',
+    [id],
+    function (error, result, fields) {
+      if (error) {
+        let responseJson = {
+          code: 20002,
+          message: 'error',
+          data: error
+        }
+        res.send(responseJson)
+      } else {
+        let responseJson = {
+          code: 20000,
+          message: 'success',
+          data: result
+        }
+        res.send(responseJson)
+      }
+    }
+  )
+})
+
+//  编辑管理员信息
+router.post('/update/edit', function (req, res, next) {
+  let id = req.body.id
+  let admin_passwd = req.body.pwd
+  let admin_token = stringRandom(16) //生成包含数字和字母的16为字符串
+  //更新密码和ID
+  DB.queryDB(
+    'UPDATE `t_admin_list` SET admin_passwd = ? , admin_token = ?  WHERE admin_id = ?',
+    [admin_passwd, admin_token, id],
+    function (error, result, fields) {
+      if (error) {
+        let responseJson = {
+          code: 20002,
+          message: 'error',
+          data: error
+        }
+        res.send(responseJson)
+      } else {
+        let responseJson = {
+          code: 20000,
+          message: 'success',
+          data: result
+        }
+        res.send(responseJson)
+      }
+    }
+  )
+})
+
+// 新增角色
+
+// 查询所有角色
+module.exports = router
---
+++ routes/login.js
@@ -0,0 +1,138 @@
+var express = require('express')
+var router = express.Router()
+var DB = require('../config/db')
+var url = require('url')
+var md5 = require('md5')
+
+var queryString = require('querystring')
+// 登录
+router.post('/login', function (req, res, next) {
+  let admin_name = req.body.username
+  let passwd = md5(req.body.password.toUpperCase())
+
+  DB.queryDB(
+    'select admin_token from `t_admin_list` where admin_login_name = ? and  admin_is_deleted = 0 limit 0,1',
+    [admin_name],
+    function (error, result, fields) {
+      if (error) {
+        let responseJson = {
+          code: 20002,
+          message: error,
+          data: '查询用户名失败'
+        }
+        res.send(responseJson)
+      } else {
+        if (result.length == 0) {
+          let responseJson = {
+            code: 20002,
+            message: result,
+            data: '该用户不存在'
+          }
+          res.send(responseJson)
+        } else {
+          DB.queryDB(
+            'select admin_token from `t_admin_list` where admin_login_name = ? and admin_pwd = ?  and admin_is_deleted = 0 limit 0,1',
+            [admin_name, passwd],
+            function (error, result, fields) {
+              if (error) {
+                let responseJson = {
+                  code: 20002,
+                  message: error,
+                  data: '查询密码失败'
+                }
+                res.send(responseJson)
+              } else {
+                if (result.length == 0) {
+                  let responseJson = {
+                    code: 20002,
+                    message: 'error',
+                    data: '密码不正确'
+                  }
+                  res.send(responseJson)
+                } else {
+                  let responseJson = {
+                    code: 20000,
+                    message: 'success',
+                    data: result
+                  }
+                  res.send(responseJson)
+                  let admin_token = result[0].admin_token
+                  DB.queryDB(
+                    'UPDATE `t_admin_list` SET admin_last_time = Now() WHERE admin_token = ?',
+                    admin_token,
+                    function (error, result, fields) {
+                      if (error) {
+                        console.log('更新失败' + error)
+                      } else {
+                        console.log('更新成功')
+                      }
+                    }
+                  )
+                }
+              }
+            }
+          )
+        }
+      }
+    }
+  )
+})
+
+// 根据token获取用户信息
+router.get('/info', function (req, res, next) {
+  let arg = url.parse(req.url).query
+  let params = queryString.parse(arg)
+  let token = params.token
+  console.log(token)
+  DB.queryDB(
+    'select admin_name,admin_id from `t_admin_list` where admin_token = ? and admin_is_deleted = 0 limit 0,1',
+    [token],
+    function (error, result, fields) {
+      if (error) {
+        let responseJson = {
+          code: 20002,
+          message: 'error',
+          data: 'token不存在'
+        }
+        res.send(responseJson)
+      } else {
+        let responseJson = {
+          code: 20000,
+          message: 'success',
+          data: result
+        }
+        res.send(responseJson)
+      }
+    }
+  )
+})
+
+// 用户登出
+router.post('/logout', function (req, res, next) {
+  //根据ID重新生成token
+  let new_token = Math.random().toString(36).substr(2)
+  let admin_id = 2
+  DB.queryDB(
+    'update `t_admin_list` set admin_token = ?  where admin_id = ?',
+    [new_token, admin_id],
+    function (error, result, fields) {
+      if (error) {
+        let responseJson = {
+          code: 20002,
+          message: 'error',
+          data: '更新token失败'
+        }
+        res.send(responseJson)
+      } else {
+        let responseJson = {
+          code: 20000,
+          message: 'success',
+          data: result
+        }
+        res.send(responseJson)
+      }
+    }
+  )
+})
+
+module.exports = router
