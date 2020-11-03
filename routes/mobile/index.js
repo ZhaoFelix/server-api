18233edf4a4cab6909dff40503a3fde3b323e2d3
zhaofelix <15021179915@163.com>
2020-11-03 09:04:48 +0800

    feat:新增移动端路由设置

---
+++ routes/mobile/index.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var router = express.Router()
+
+router.get('/',function(req,res,next){
+   res.send({
+       "code":400
+   })
+});
+
+
+module.exports = router
+
+
