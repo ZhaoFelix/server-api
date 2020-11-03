875d41905d0de0ed5cf6d2bac15c48b70cb65569
zhaofelix <15021179915@163.com>
2020-11-03 14:24:15 +0800

    feat:移动端测试路由说明

--- routes/v1.0/mobile/index.js
+++ routes/v1.0/mobile/index.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/admin',function(req,res,next){
+// 只用于移动端路由连接验证测试，不添加相关功能
+router.get('/',function(req,res,next){
    res.send({
-       "code":400
+       "code":"移动端测试路由"
    })
 });
 module.exports = router
