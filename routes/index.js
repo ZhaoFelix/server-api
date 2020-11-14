e5b3ae15d0c17d628bf07548d4d5af7f3c30ff4a
zhaofelix <15021179915@163.com>
2020-11-14 20:39:49 +0800

    feat:增加接口错误处理中间件

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var DB = require('../config/db')
 var helper = require('../utils/helper')
+const boom = require("boom")
+const { CODE_ERROR } = require('../utils/constant')
 //  扫描路由路径
 const scanResult = helper.scanDirModules(__dirname,__filename)
 for (const prefix in scanResult) {
@@ -9,6 +11,7 @@ for (const prefix in scanResult) {
     router.use(prefix,scanResult[prefix])
   }
 } 
+
 /* 仅用于服务器连接和数据库连接测试，不涉及相关功能 */
 router.get('/', function(req, res, next) {
   DB.queryDB("select * from  t_user_list",function(error,results,fields){
@@ -20,5 +23,32 @@ router.get('/', function(req, res, next) {
   })
   res.render('index', { title: 'Express' });
 });
+
+/**
+ *  集中处理404请求的中间件
+ * */ 
+router.use((req,res,next) => {
+  next(boom.notFound('接口不存在'))
+})
+
+
+/** 
+ *  自定义路由异常处理中间件
+ *  注意点：
+ *  第一，方法的参数不能减少
+ *  第二，方法必须放在路由的最后面
+ * **/
+
+ router.use((err, req, res, next) => {
+    const  msg = (err && err.message) || '系统错误'
+    const statusCode = (err.output && err.output.statusCode) || 500;
+    const errorMsg = (err.output && err.output.payload && err.output.payload.error) || err.message
+    res.status(statusCode).json({
+      code:CODE_ERROR,
+      msg,
+      error:statusCode,
+      errorMsg
+    })
+ })
 
 module.exports = router;
