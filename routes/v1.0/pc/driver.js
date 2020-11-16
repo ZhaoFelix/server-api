be6c572a94e8069262bbf1b2a9a3e6261794d176
zhaofelix <15021179915@163.com>
2020-11-16 16:04:55 +0800

    feat:增加查询当日值班司机的接口

--- routes/v1.0/pc/driver.js
+++ routes/v1.0/pc/driver.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 var DB = require('../../../config/db')
 var url = require('url')
+const { resolve } = require('path')
 
 // 查询所有司机的信息
 router.get("/query/all",function(req,res,next){
@@ -57,5 +58,46 @@ router.get('/query/schedule',function(req,res,next){
       res.send(responseJson)
     }
   })
+})
+
+// 查询当天的值班司机信息
+router.get('/query/current',function(req,res,next){
+  let date = new Date()
+  let current_month = date.getMonth() + 1
+  let current_date = date.getDate()
+  return new Promise((resolve,reject) => {
+    DB.queryDB('select  * from t_driver_schedule where schedule_month = ?',[current_month],function(error,result,fields){
+      if (error) {
+        reject('查询信息失败，error:'+error)
+      } else {
+       resolve(result)
+      }
+    })
+  }).then((data) => {
+    let returnResult = []
+    for (var i=0;i<data.length;i++){
+      let obj = data[i]
+      let schedule = obj.driver_schedule.split('.')
+      console.log(current_date.toString())
+      if(schedule.indexOf(current_date.toString()) == -1) {
+        returnResult.push(obj)
+      }
+    }
+    let responseJson = {
+      code: 20000,
+      message: 'success',
+      data: returnResult
+    }
+    res.send(responseJson)
+  })
+  .catch((error) => {
+    let responseJson = {
+      code: 20002,
+      message: 'error',
+      data: error
+    }
+    res.send(responseJson)
+  })
+  
 })
 module.exports = router
