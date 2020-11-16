6df58d713fb9a0bae2fd150299ad77f1f248ec01
ammi3 <758217673@qq.com>
2020-11-16 10:36:02 +0800

    feat:完善driver接口

--- routes/v1.0/mobile/driver.js
+++ routes/v1.0/mobile/driver.js
@@ -34,10 +34,11 @@ router.get('/search', function(req, res, next) {
     )
 })
 
-// 显示所有订单--不完善
+// 显示所有订单-全部
 router.get('/order/query/all', function(req, res, next) {
+    let driver_id = req.body.driver_id
     DB.queryDB(
-        'select * from `t_order_list` where order_is_deleted = 0', [],
+        'select * from `t_order_list` where driver_id = ? and (order_is_deleted = 1 or order_status = 1)', [driver_id],
         function(error, result, fields) {
             if(error) {
                 let responseJson = {
@@ -58,23 +59,50 @@ router.get('/order/query/all', function(req, res, next) {
     )
 });
 
-// 司机接单
-router.get('/order/get', function(req, res, next) {
-    let order_id = req.body.order_id
+// 显示全部订单-已完成
+router.get('/order/query/all/finished', function(req, res, next) {
+    let driver_id = req.body.driver_id
     DB.queryDB(
-        'update `t_order_list` set driver_id = ? where order_id = ? and order_is_deleted = 0',[driver_id, order_id],
+        'select * from `t_order_list` where driver_id = ? and order_status = 1', [driver_id],
         function(error, result, fields) {
             if(error) {
                 let responseJson = {
                     code: 20002,
-                    message: '接单失败',
+                    message: 'error',
                     data: error
                 }
                 res.send(responseJson)
             } else {
                 let responseJson = {
                     code: 20000,
-                    message: '接单成功',
+                    message: 'success',
+                    data: result
+                }
+                res.send(responseJson)
+            }
+        }
+    )
+})
+
+// 更新订单状态
+router.post('/order/update', function(req, res, next) {
+    let order_status = req.body.order_status
+    let order_id = req.body.order_id
+    // console.log(order_id, order_status)
+    DB.queryDB(
+        'update `t_order_list` set order_status = ? where order_id = ? and order_is_deleted = 0',[order_status, order_id],
+        function(error, result, fields) {
+            if(error) {
+                let responseJson = {
+                    code: 20002,
+                    message: '更新订状态单失败',
+                    data: error
+                }
+                res.send(responseJson)
+            } else {
+                let responseJson = {
+                    code: 20000,
+                    message: '更新订单状态成功',
                     data: result
                 }
                 res.send(responseJson)
@@ -82,4 +110,47 @@ router.get('/order/get', function(req, res, next) {
         }
     )
 });
+
+//确认信息
+router.get('/info', function(req, res, next) {
+    let driver_id = req.body.driver_id
+    DB.queryDB(
+        'select * from `t_driver_list` where driver_id = ? and driver_is_deleted = 0',[driver_id],
+        function(error, result, fields) {
+            if(error) {
+                let responseJson = {
+                    code: 20002,
+                    message: 'error',
+                    data: error
+                }
+                res.send(responseJson)
+            } else {
+                var dataString = JSON.stringify(result);
+                var data = JSON.parse(dataString);
+                console.log(data)
+                let {car_id} = data[0]
+                DB.queryDB(
+                    'select * from `t_car_list` where car_id = ? and car_is_deleted = 0',[car_id],
+                    function(error, result, fields) {
+                        if(error) {
+                            let responseJson = {
+                                code: 20002,
+                                message: 'error',
+                                data: error 
+                            } 
+                            res.send(responseJson)
+                        } else {
+                            let responseJson = {
+                                code: 20000,
+                                message: 'success',
+                                data: result
+                            }
+                            res.send(responseJson)
+                        }
+                    }
+                )
+            }
+        }
+    )
+})
 module.exports = router
