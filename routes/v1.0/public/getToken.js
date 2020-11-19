bbd953dbdd771e4660273912567e4d239bb3c9fb
ammi3 <758217673@qq.com>
2020-11-19 18:37:15 +0800

    feat:OSSC

--- routes/v1.0/public/getToken.js
+++ routes/v1.0/public/getToken.js
@@ -1,44 +1,43 @@
 const OSS = require('ali-oss')
-const STS = OSS.STS
 const express = require('express');
 const router = express();
+const crypto = require("crypto")
 
-
-const stsClient = new STS({
-  accessKeyId: 'LTAIhIVj0cdX1Fxh',
-  accessKeySecret: 'm8l0EIM1ij4R60YtIiLGmwp6b28lx1',
-  bucket: 'supers1'
-});
-
-async function getToken() {
-    const STS_ROLE = '<STS_ROLE>'  // 指定角色的ARN。格式：acs:ram::$accountID:role/$roleName。
-    const STSpolicy = {
-      Statement: [
-        {
-          Action: ['oss:*'],
-          Effect: 'Allow',
-          Resource: ['acs:oss:*:*:*']
-        }
-      ],
-      Version: '1'
-    };
-    const result = await stsClient.assumeRole(
-      STS_ROLE,
-      STSpolicy,
-      3600 // STS过期时间，单位：秒。
-    );
-    const { credentials } = result;
-
-    return credentials;
+const router = express()
+const config = {
+    dirPath: 'xxx/', // 存放到哪个目录下
+    bucket: '你的 Bucket 的名称',
+    region: 'oss-cn-hangzhou',// 我的是 hangzhou
+    accessKeyId: '你的 key',
+    accessKeySecret: '你的密码',
+    expAfter: 300000, // 签名失效时间，毫秒
+    maxSize: 1048576000 // 文件最大的 size
 }
 
-router.get('/getToken', async (req, res) => {
-  // 获取STS。
-  const credentials = await getToken()
-  console.log(credentials.AccessKeyId)
-  console.log(credentials.AccessKeySecret)
-  console.log(credentials.SecurityToken)
-  res.json(credentials);
+router.get('/osstoken', (req, res) => {
+    const host = `https://${config.bucket}.${config.region}.aliyuncs.com`
+    const expireTime = new Date().getTime() + config.expAfter
+    const expiration = new Date(expireTime).toISOString()
+    const policyString = JSON.stringify({
+        expiration,
+        conditions: [
+            ['content-length-range', 0, config.maxSize],
+            ['starts-with', '$key', config.dirPath]
+        ]
+    })
+    const policy = Buffer.from(policyString).toString('base64')
+
+    const signature = crypto.createHmac('sha1', config.accessKeySecret).update(policy).digest("base64")
+
+    res.json({
+        signature,
+        policy,
+        host,
+        'OSSAccessKeyId': config.accessKeyId,
+        'key': expireTime,
+        'success_action_status': 201,
+        'dirPath': config.dirPath,
+    })
 })
 
 module.exports = router
