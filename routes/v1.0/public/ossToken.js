8081b97f4ed151dc1f618fc046b4da72f8c5fcc6
zhaofelix <15021179915@163.com>
2020-12-03 09:53:46 +0800

    fix:osskoen响应

--- routes/v1.0/public/ossToken.js
+++ routes/v1.0/public/ossToken.js
@@ -5,15 +5,19 @@ const ossHelper = require("../../../utils/ossHelper")
 
 router.get("/getOssToken",(req,res) => {
     const myHelper = new ossHelper({
-        accessKey:config.oss_config.accessKeyId,
+        accessKeyId:config.oss_config.accessKeyId,
         accessKeySecret:config.oss_config.accessKeySecret,
         timeOut:1, // 有效时长1小时
         maxSize:10 // 限制文件大小（单位：MB）
     })
     // 生成参数
     const params = myHelper.createUploadParams();
-    res.json(params)
-})
 
+    res.json({
+        code:"20000",
+        message:"获取osstoken成功",
+        data:params
+    })
+})
 
 module.exports = router
