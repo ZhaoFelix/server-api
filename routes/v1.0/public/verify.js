27925338a0c5cec7f5eff3b548b58117c5f29205
zhaofelix <15021179915@163.com>
2020-12-08 09:08:19 +0800

    feat:物业认证接口拆分

--- routes/v1.0/public/verify.js
+++ routes/v1.0/public/verify.js
@@ -2,7 +2,7 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2020-12-01 07:54:40
- * @LastEditTime: 2020-12-07 20:17:59
+ * @LastEditTime: 2020-12-08 09:07:49
  * @FilePath: /server-api/routes/v1.0/public/verify.js
  * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
  */
@@ -35,8 +35,8 @@ router.get('/verify', function (req, res, next) {
             }
         })
     },function(err){
-       console.log(err);
-       let responseJson = {
+    console.log(err);
+    let responseJson = {
         code: 20002,
         message: 'error',
         data: err
@@ -92,17 +92,38 @@ router.post('/checked', function(req, res, next) {
             resolve(result)
         }
     })
-   }).then((data) => {
-       if(data.length == 0){
+}).then((data) => {
+    if(data.length == 0){
             // 记录不存在,无法进行认证
             let responseJson = {
                 code: 20001,
                 message: '手机号不存在'
             }
             res.send(responseJson)
-       } else {
-        // 存在记录更新角色类型为1
-       return new Promise((resolve,reject) => {
+    } else {
+        let responseJson = {
+            code: 20000,
+            message: '查询成功',
+            data:data
+        }
+        res.send(responseJson)
+    }
+})
+.catch((error) => {
+    let responseJson = {
+        code: 20002,
+        message: 'error',
+        data: error
+    }
+    res.send(responseJson);
+})
+})
+
+// 认证更新状态
+router.post("/auth",function(req,res,next){
+    let {phone,userId} = req.body
+       // 存在记录更新角色类型为1
+    return new Promise((resolve,reject) => {
         DB.queryDB("update  t_user_list set  user_type = 1 where  user_id = ?",userId,function(error,result,fields){
             if (error){
                 reject("更新用户角色失败，error"+error)
@@ -110,50 +131,68 @@ router.post('/checked', function(req, res, next) {
                 resolve(data)
             }
         })
-       })
-       }
-   })
-   .then((data) => {
-     // 更新状态为已认证
-     return new Promise((resolve,reject) => {
-         DB.queryDB("update  t_estate_list set  estate_is_auth = 1,wechat_id = ?, estate_wechat_time = NOW() where  estate_is_deleted = 0  and estate_is_auth = 0 and estate_phone = ?",
-         [userId,phone],function(error,result,fields){
-             if (error) {
-                reject("更新角色状态失败，error"+error)
-             } else {
-                 resolve(result)
-             }
-         })
-     })  
-   })
-   .then((data) => {
-        // 根据手机号查询信息
-       return new Promise((resolve,reject) => {
-        DB.queryDB("select estate_id,estate_name,estate_phone,estate_card_id,estate_gender,estate_company,estate_region,estate_plot from t_estate_list where  estate_phone = ?",[phone],function(error,result,fields){
-            if (error) {
-                reject("手机号查询信息失败，error"+error)
+    })
+    .then((data) => {
+        // 更新状态为已认证
+        return new Promise((resolve,reject) => {
+        DB.queryDB("update  t_estate_list set  estate_is_auth = 1,wechat_id = ?, estate_wechat_time = NOW() where  estate_is_deleted = 0  and estate_is_auth = 0 and estate_phone = ?",
+        [userId,phone],function(error,result,fields){
+        if (error) {
+            reject("更新角色状态失败，error"+error)
             } else {
                 resolve(result)
             }
-        })
-       })
-   })
-   .then((data) => {
-    let responseJson = {
-        code: 20000,
-        message: '认证成功',
-        data:data
-    }
-    res.send(responseJson)
-   })
-   .catch((error) => {
-    let responseJson = {
-        code: 20002,
-        message: 'error',
-        data: error
-    }
-    res.send(responseJson);
-   })
+            })
+        }) 
+    })
+    .then((data) => {
+        let responseJson = {
+            code: 20000,
+            message: '认证成功',
+            data:data
+        }
+        res.send(responseJson)
+    })
+    .catch((error) => {
+        let responseJson = {
+            code: 20002,
+            message: 'error',
+            data: error
+        }
+        res.send(responseJson);
+    })
 })
 
+// 根据手机号获取物业信息
+router.get("/estate",function(req,res,next){
+    let parseObj = url.parse(req.url, true) // 将URL解析为一个对象
+    req.query = parseObj.query;
+    let phone = req.query.phone;
+    // 根据手机号查询信息
+    return new Promise((resolve,reject) => {
+        DB.queryDB("select estate_id,estate_name,estate_phone,estate_card_id,estate_gender,estate_company,estate_region,estate_plot from t_estate_list where  estate_phone = ?",[phone],function(error,result,fields){
+                if (error) {
+                    reject("手机号查询信息失败，error"+error)
+                } else {
+                    resolve(result)
+                }
+            })
+    })
+    .then((data) => {
+        let responseJson = {
+            code: 20000,
+            message: '认证成功',
+            data:data
+        }
+        res.send(responseJson)
+    })
+    .catch((error) =>{
+        let responseJson = {
+            code: 20002,
+            message: 'error',
+            data: error
+        }
+        res.send(responseJson);
+    })
+})
 module.exports = router;
