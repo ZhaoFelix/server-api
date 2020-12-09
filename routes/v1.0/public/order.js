88d6fee80957d3bd12cf6c9cf7411e28d469edae
zhaofelix <15021179915@163.com>
2020-12-09 13:45:25 +0800

    feat:支付回调测试

--- routes/v1.0/public/order.js
+++ routes/v1.0/public/order.js
@@ -2,7 +2,7 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2020-11-17 08:57:51
- * @LastEditTime: 2020-12-09 13:37:28
+ * @LastEditTime: 2020-12-09 13:44:50
  * @FilePath: /server-api/routes/v1.0/public/order.js
  * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
  */
@@ -67,11 +67,11 @@ router.post(
       var jsonData = req.body.xml
       if (jsonData.result_code == 'SUCCESS') {
         //支付成功，更新订单状态
-        let tradeNo = jsonData.out_trade_no + ''
+        let tradeNo = jsonData.out_trade_no
         let order_final_price = jsonData.total_fee
         console.log(jsonData)
         DB.queryDB(
-          'UPDATE `t_order_list` SET order_status=1,order_pay_time=NOW(),order_final_price = ? WHERE order_number = ? AND order_status=0',
+          "UPDATE t_order_list SET order_status=1,order_pay_time=NOW(),order_final_price = ? WHERE order_number = '?' AND order_status=0",
           [tradeNo,order_final_price],
           function (error, result, fields) {
             console.log('UPDATE t_order_list SET order_status=1,order_pay_time=NOW(),order_final_price = '+order_final_price+' WHERE order_number = '+tradeNo+' AND order_status=0')
