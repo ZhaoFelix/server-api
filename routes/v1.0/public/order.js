cb011eb65fd5802fccae45681aa8c04773db469a
zhaofelix <15021179915@163.com>
2020-12-09 13:52:28 +0800

    fix:代码修复

--- routes/v1.0/public/order.js
+++ routes/v1.0/public/order.js
@@ -2,7 +2,7 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2020-11-17 08:57:51
- * @LastEditTime: 2020-12-09 13:46:36
+ * @LastEditTime: 2020-12-09 13:51:09
  * @FilePath: /server-api/routes/v1.0/public/order.js
  * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
  */
@@ -71,8 +71,8 @@ router.post(
         let order_final_price = jsonData.total_fee
         console.log(jsonData)
         DB.queryDB(
-          "UPDATE t_order_list SET order_status=1,order_pay_time=NOW(),order_final_price = ? WHERE order_number = '?' AND order_status=0",
-          [tradeNo,order_final_price],
+          "UPDATE t_order_list SET order_status=1,order_pay_time=NOW(),order_final_price = ? WHERE order_number = ? AND order_status=0",
+          [order_final_price,tradeNo],
           function (error, result, fields) {
             if (error) {
               console.log(tradeNo + '订单更新失败,错误原因：' + error)
