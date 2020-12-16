58d3e58da2353f796e58edfdbb35e7ca04327086
zhaofelix <15021179915@163.com>
2020-12-16 10:56:42 +0800

    feat:增加商业下单接口

--- routes/v1.0/pc/operatedOrder.js
+++ routes/v1.0/pc/operatedOrder.js
@@ -9,186 +9,196 @@ var url = require('url')
 const { type } = require('os')
 
 // TODO:根据线路类型查询今日值班的司机
-router.get('/driver/query',function(req,res,next){
-    let date = new Date()
-    let current_month = date.getMonth() + 1
-    let current_date = date.getDate()
-    let parseObj = url.parse(req.url,true)
-    let query = parseObj.query
-    let router_type = parseInt(query.type);
-    return new Promise((resolve,reject) => {
-        if (router_type == 4) {
-            DB.queryDB('SELECT driver_name,car_router_type,router_note,driver_id,driver_is_substitutes from t_driver_list where car_router_type = ? and driver_id not in (SELECT driver_id from t_order_list WHERE order_status = 3 and order_status = 4)',[router_type],
-            function(error,result,fields) {
-                if (error) {
-                    reject('查询信息失败，error:'+error)
-                  } else {
-                   resolve(result)
-                  }
-            }
-            )
-        }
-        else {
-            DB.queryDB('select  driver_name,router_type,driver_schedule,router_note,driver_id,driver_is_substitutes from t_driver_schedule where schedule_month = ? and router_type = ? and driver_id not in (SELECT driver_id from t_order_list WHERE order_status != 3 and order_status != 4  and driver_id is not null)',[current_month,router_type],function(error,result,fields){
-                if (error) {
-                  reject('查询信息失败，error:'+error)
-                } else {
-                 resolve(result)
-                }
-              })
-        }
-       
-      }).then((data) => {
-        let returnResult = []
-        if (router_type == 4){
-            returnResult = data
-            console.log(returnResult)
-        } else {
-            for (var i=0;i<data.length;i++){
-                let obj = data[i]
-                let schedule = obj.driver_schedule.split('.')
-                if(schedule.indexOf(current_date.toString()) == -1) {
-                  returnResult.push(obj)
-                }
-              }
-        }
-        
-        
-        let responseJson = {
-            code: 20000,
-            message: 'success',
-            data: returnResult
+router.get('/driver/query', function (req, res, next) {
+  let date = new Date()
+  let current_month = date.getMonth() + 1
+  let current_date = date.getDate()
+  let parseObj = url.parse(req.url, true)
+  let query = parseObj.query
+  let router_type = parseInt(query.type)
+  return new Promise((resolve, reject) => {
+    if (router_type == 4) {
+      DB.queryDB(
+        'SELECT driver_name,car_router_type,router_note,driver_id,driver_is_substitutes from t_driver_list where car_router_type = ? and driver_id not in (SELECT driver_id from t_order_list WHERE order_status = 3 and order_status = 4)',
+        [router_type],
+        function (error, result, fields) {
+          if (error) {
+            reject('查询信息失败，error:' + error)
+          } else {
+            resolve(result)
           }
-          res.send(responseJson)
-    
-       
-      })
-      .catch((error) => {
-        let responseJson = {
-          code: 20002,
-          message: 'error',
-          data: error
         }
-        res.send(responseJson)
-      })
+      )
+    } else {
+      DB.queryDB(
+        'select  driver_name,router_type,driver_schedule,router_note,driver_id,driver_is_substitutes from t_driver_schedule where schedule_month = ? and router_type = ? and driver_id not in (SELECT driver_id from t_order_list WHERE order_status != 3 and order_status != 4  and driver_id is not null)',
+        [current_month, router_type],
+        function (error, result, fields) {
+          if (error) {
+            reject('查询信息失败，error:' + error)
+          } else {
+            resolve(result)
+          }
+        }
+      )
+    }
+  })
+    .then((data) => {
+      let returnResult = []
+      if (router_type == 4) {
+        returnResult = data
+        console.log(returnResult)
+      } else {
+        for (var i = 0; i < data.length; i++) {
+          let obj = data[i]
+          let schedule = obj.driver_schedule.split('.')
+          if (schedule.indexOf(current_date.toString()) == -1) {
+            returnResult.push(obj)
+          }
+        }
+      }
 
+      let responseJson = {
+        code: 20000,
+        message: 'success',
+        data: returnResult
+      }
+      res.send(responseJson)
+    })
+    .catch((error) => {
+      let responseJson = {
+        code: 20002,
+        message: 'error',
+        data: error
+      }
+      res.send(responseJson)
+    })
 })
 
-// TODO:根据司机的类型查询车辆信息
-// router.get('/car/query/',function(req,res,next){
-//     // 如果是替班司机
-//     // 非替班司机
-// })
-
 // TODO:查询实时订单
-router.get('/order/query/',function(req,res,next){
-    let parseObj = url.parse(req.url,true)
-    let query = parseObj.query
-    let limit = parseInt(query.limit)
-    let offset = parseInt(query.offset)
-    // 查询所有未支付和未派发的订单
-    DB.queryDB('select  * from v_no_assign_order',[limit,offset],function(error,result,fields){
-        if (error) {
-            let responseJson = {
-              code: 20002,
-              message: error,
-              data: '查询订单失败'
-            }
-            res.send(responseJson)
-          } else {
-              
-            let responseJson = {
-                code: 20000,
-                message: "sucess",
-                data: result
-              }
-              res.send(responseJson)
-        } 
-    })
+router.get('/order/query/', function (req, res, next) {
+  let parseObj = url.parse(req.url, true)
+  let query = parseObj.query
+  let limit = parseInt(query.limit)
+  let offset = parseInt(query.offset)
+  // 查询所有未支付和未派发的订单
+  DB.queryDB(
+    'select  * from v_no_assign_order',
+    [limit, offset],
+    function (error, result, fields) {
+      if (error) {
+        let responseJson = {
+          code: 20002,
+          message: error,
+          data: '查询订单失败'
+        }
+        res.send(responseJson)
+      } else {
+        let responseJson = {
+          code: 20000,
+          message: 'sucess',
+          data: result
+        }
+        res.send(responseJson)
+      }
+    }
+  )
 })
 
 // 根据路线类型查询车辆
-router.get('/car/query',function(req,res,next){
-    let parseObj = url.parse(req.url,true)
-    let query = parseObj.query
-    let router_type = parseInt(query.type);
-    return new Promise((resolve,reject) => {
-            DB.queryDB('SELECT * from t_car_list where car_router_type = ? and car_id not in (SELECT car_id from t_order_list WHERE order_status = 3 and order_status = 4)',[router_type],function(error,result,fields){
-                if (error) {
-                  reject('查询信息失败，error:'+error)
-                } else {
-                 resolve(result)
-                }
-              })
-      }).then((data) => {  
-        let responseJson = {
-            code: 20000,
-            message: 'success',
-            data: data
-          }
-          res.send(responseJson)
-      })
-      .catch((error) => {
-        let responseJson = {
-          code: 20002,
-          message: 'error',
-          data: error
+router.get('/car/query', function (req, res, next) {
+  let parseObj = url.parse(req.url, true)
+  let query = parseObj.query
+  let router_type = parseInt(query.type)
+  return new Promise((resolve, reject) => {
+    DB.queryDB(
+      'SELECT * from t_car_list where car_router_type = ? and car_id not in (SELECT car_id from t_order_list WHERE order_status = 3 and order_status = 4)',
+      [router_type],
+      function (error, result, fields) {
+        if (error) {
+          reject('查询信息失败，error:' + error)
+        } else {
+          resolve(result)
         }
-        res.send(responseJson)
-      })
-
+      }
+    )
+  })
+    .then((data) => {
+      let responseJson = {
+        code: 20000,
+        message: 'success',
+        data: data
+      }
+      res.send(responseJson)
+    })
+    .catch((error) => {
+      let responseJson = {
+        code: 20002,
+        message: 'error',
+        data: error
+      }
+      res.send(responseJson)
+    })
 })
 
 // 调度员取消未支付的订单
-router.get('/order/cancel',function(req,res,next){
-    let parseObj = url.parse(req.url,true)
-    let query = parseObj.query
-    let order_id = parseInt(query.order_id);
-    // 将订单的状态更新为2
-    DB.queryDB('update  t_order_list set order_status = 2 where order_id = ? and  order_is_deleted = 0 and order_status = 0',[order_id],function(error,result,fields){
-        if(error){
-            let responseJson = {
-                code: 20002,
-                message: '取消订单失败，请重试！',
-                data: error
-              }
-              res.send(responseJson)
-        } else {
-            let responseJson = {
-                code: 20000,
-                message: 'success',
-                data: result
-              }
-              res.send(responseJson)
-            }
-    })
+router.get('/order/cancel', function (req, res, next) {
+  let parseObj = url.parse(req.url, true)
+  let query = parseObj.query
+  let order_id = parseInt(query.order_id)
+  // 将订单的状态更新为2
+  DB.queryDB(
+    'update  t_order_list set order_status = 2 where order_id = ? and  order_is_deleted = 0 and order_status = 0',
+    [order_id],
+    function (error, result, fields) {
+      if (error) {
+        let responseJson = {
+          code: 20002,
+          message: '取消订单失败，请重试！',
+          data: error
+        }
+        res.send(responseJson)
+      } else {
+        let responseJson = {
+          code: 20000,
+          message: 'success',
+          data: result
+        }
+        res.send(responseJson)
+      }
+    }
+  )
 })
 
 // 针对订单指派司机和车辆
-router.get('/order/assign',function(req,res,next){
-    let parseObj = url.parse(req.url,true)
-    let query = parseObj.query
-    let order_id = parseInt(query.order_id);
-    let car_id = parseInt(query.car_id);
-    let driver_id = parseInt(query.driver_id);
-    // 将订单的状态更新为3
-    console.log("测试")
-    DB.queryDB('update  t_order_list set order_status = 3, car_id = ? , driver_id = ? where order_id = ? and  order_is_deleted = 0 and order_status = 1',[car_id,driver_id,order_id],function(error,result,fields){
-        if(error){
-            let responseJson = {
-                code: 20002,
-                message: '指派订单失败，请重试！',
-                data: error
-              }
-              res.send(responseJson)
-        } else {
-            let responseJson = {
-                code: 20000,
-                message: 'success',
-                data: result
-              }
-              res.send(responseJson)
-            }
-    })
+router.get('/order/assign', function (req, res, next) {
+  let parseObj = url.parse(req.url, true)
+  let query = parseObj.query
+  let order_id = parseInt(query.order_id)
+  let car_id = parseInt(query.car_id)
+  let driver_id = parseInt(query.driver_id)
+  // 将订单的状态更新为3
+  console.log('测试')
+  DB.queryDB(
+    'update  t_order_list set order_status = 3, car_id = ? , driver_id = ? where order_id = ? and  order_is_deleted = 0 and order_status = 1',
+    [car_id, driver_id, order_id],
+    function (error, result, fields) {
+      if (error) {
+        let responseJson = {
+          code: 20002,
+          message: '指派订单失败，请重试！',
+          data: error
+        }
+        res.send(responseJson)
+      } else {
+        let responseJson = {
+          code: 20000,
+          message: 'success',
+          data: result
+        }
+        res.send(responseJson)
+      }
+    }
+  )
 })
 module.exports = router
--- routes/v1.0/public/order.js
+++ routes/v1.0/public/order.js
@@ -2,101 +2,203 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2020-11-17 08:57:51
- * @LastEditTime: 2020-12-10 08:56:35
+ * @LastEditTime: 2020-12-16 10:44:08
  * @FilePath: /server-api/routes/v1.0/public/order.js
  * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
  */
 var express = require('express')
 var router = express.Router()
 var DB = require('../../../config/db')
-var wxpay = require('../../../utils/wxpay') 
+var wxpay = require('../../../utils/wxpay')
 var xmlparser = require('express-xml-bodyparser')
 var config = require('../../../config/env')
 let common = require('../../../utils/common')
-
+let util = require('../../../utils/pay.js')
 const mch = config.mch
 // 微信支付
-router.post('/wxpay',function(req,res,next){
-    let {userId,userType,address,buildArea,imagesList,isFirst,name,openId,orderNote,orderPrice,phoneNumber,selectTime,subAddress,orderType} = req.body;
-    let appId = mch.appId
-    let notify_url = mch.notify_url
-    let ip = mch.ip
-    let attach = mch.attach
-    let body = mch.body
-    // TODO:价格待添加计算方式
-    let money =  1 //common.clocPrice(buildArea,isFirst,userType)
-    wxpay.order(appId,attach,body,openId,money,notify_url,ip)
+router.post('/wxpay', function (req, res, next) {
+  let {
+    userId,
+    userType,
+    address,
+    buildArea,
+    imagesList,
+    isFirst,
+    name,
+    openId,
+    orderNote,
+    orderPrice,
+    phoneNumber,
+    selectTime,
+    subAddress,
+    orderType
+  } = req.body
+  let appId = mch.appId
+  let notify_url = mch.notify_url
+  let ip = mch.ip
+  let attach = mch.attach
+  let body = mch.body
+  // TODO:价格待添加计算方式
+  let money = 1 //common.clocPrice(buildArea,isFirst,userType)
+  wxpay
+    .order(appId, attach, body, openId, money, notify_url, ip)
     .then((result) => {
-        DB.queryDB('INSERT INTO t_order_list (user_id,order_price,user_reserve_time,order_size,order_user_type,order_number, user_phone,user_address,user_is_first,user_note,order_user_name,order_type,order_created_time) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,NOW())',
-             [userId,orderPrice,selectTime,buildArea,userType,result.tradeNo,phoneNumber,address+subAddress,isFirst,orderNote,name,orderType],function(error,re,fields){
-            if (error) {
-                let responseJson = {
-                    code: 20002,
-                    message: '创建订单失败',
-                    data: error
-                  }
-                  res.send(responseJson)
-            } else {
-                let responseJson = {
-                    code: 20000,
-                    message: '支付配置成功',
-                    data: result
-                  }
-                  res.send(responseJson)
-                 let order_id = re.insertId
-                //  TODO:订单创建成功后将用户提交的图片链接存储到t_order_info_list表
-                DB.queryDB("insert  into t_order_info_list (user_place_order_img,user_place_order_time,order_id,created_time) values (?,Now(),?,NOW())",[JSON.stringify(imagesList),order_id],function(error,resu,fields){
-                  if (error) {
-                      console.log("新建订单信息记录失败，error:"+error)
-                  } else {
-                      console.log("新建订单信息记录成功，记录ID为："+resu.insertId)
-                  }
-                })
+      DB.queryDB(
+        'INSERT INTO t_order_list (user_id,order_price,user_reserve_time,order_size,order_user_type,order_number, user_phone,user_address,user_is_first,user_note,order_user_name,order_type,order_created_time) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,NOW())',
+        [
+          userId,
+          orderPrice,
+          selectTime,
+          buildArea,
+          userType,
+          result.tradeNo,
+          phoneNumber,
+          address + subAddress,
+          isFirst,
+          orderNote,
+          name,
+          orderType
+        ],
+        function (error, re, fields) {
+          if (error) {
+            let responseJson = {
+              code: 20002,
+              message: '创建订单失败',
+              data: error
             }
-        })
+            res.send(responseJson)
+          } else {
+            let responseJson = {
+              code: 20000,
+              message: '支付配置成功',
+              data: result
+            }
+            res.send(responseJson)
+            let order_id = re.insertId
+            //  订单创建成功后将用户提交的图片链接存储到t_order_info_list表
+            DB.queryDB(
+              'insert  into t_order_info_list (user_place_order_img,user_place_order_time,order_id,created_time) values (?,Now(),?,NOW())',
+              [JSON.stringify(imagesList), order_id],
+              function (error, resu, fields) {
+                if (error) {
+                  console.log('新建订单信息记录失败，error:' + error)
+                } else {
+                  console.log(
+                    '新建订单信息记录成功，记录ID为：' + resu.insertId
+                  )
+                }
+              }
+            )
+          }
+        }
+      )
     })
     .catch((error) => {
-        let responseJson = {
-            code: 20002,
-            message: '支付配置失败',
-            data: error
-          }
-          res.send(responseJson)
+      let responseJson = {
+        code: 20002,
+        message: '支付配置失败',
+        data: error
+      }
+      res.send(responseJson)
     })
-
 })
 
-// TODO:微信支付回调，更新订单的状态、更新最终金额、更新订单的支付时间
+// 微信支付回调，更新订单的状态、更新最终金额、更新订单的支付时间
 router.post(
-    '/callback',
-    xmlparser({ trim: false, explicitArray: false }),
-    function (req, res) {
-      var jsonData = req.body.xml
-      if (jsonData.result_code == 'SUCCESS') {
-        //支付成功，更新订单状态
-        let tradeNo = jsonData.out_trade_no
-        let order_final_price = jsonData.total_fee
-        console.log(jsonData)
+  '/callback',
+  xmlparser({ trim: false, explicitArray: false }),
+  function (req, res) {
+    var jsonData = req.body.xml
+    if (jsonData.result_code == 'SUCCESS') {
+      //支付成功，更新订单状态
+      let tradeNo = jsonData.out_trade_no
+      let order_final_price = jsonData.total_fee
+      console.log(jsonData)
+      DB.queryDB(
+        'UPDATE t_order_list SET order_status=1,order_pay_time=NOW(),order_final_price = ? WHERE order_number = ? AND order_status=0',
+        [order_final_price / 100, tradeNo],
+        function (error, result, fields) {
+          if (error) {
+            console.log(tradeNo + '订单更新失败,错误原因：' + error)
+          } else {
+            console.log(tradeNo + '订单更新成功')
+          }
+        }
+      )
+    } else {
+      //失败
+      console.log(
+        '当前订单的支付状态，result_code:' +
+          jsonData.result_code +
+          'return_code:' +
+          jsonData.return_code
+      )
+    }
+  }
+)
+
+// 商业下单，只创建订单配置支付信息
+router.post('/business', function (req, res, next) {
+  let {
+    userId,
+    userType,
+    address,
+    buildArea,
+    imagesList,
+    isFirst,
+    name,
+    orderNote,
+    phoneNumber,
+    selectTime,
+    subAddress,
+    orderType
+  } = req.body
+  let tradeNo = util.getTradeId('mp')
+  DB.queryDB(
+    'INSERT INTO t_order_list (user_id,user_reserve_time,order_size,order_user_type,order_number, user_phone,user_address,user_is_first,user_note,order_user_name,order_type,order_created_time) VALUES (?,?,?,?,?,?,?,?,?,?,?,NOW())',
+    [
+      userId,
+
+      selectTime,
+      buildArea,
+      userType,
+      tradeNo,
+      phoneNumber,
+      address + subAddress,
+      isFirst,
+      orderNote,
+      name,
+      orderType
+    ],
+    function (error, re, fields) {
+      if (error) {
+        let responseJson = {
+          code: 20002,
+          message: '创建订单失败',
+          data: error
+        }
+        res.send(responseJson)
+      } else {
+        let responseJson = {
+          code: 20000,
+          message: '下单成功',
+          data: re
+        }
+        res.send(responseJson)
+        let order_id = re.insertId
         DB.queryDB(
-          "UPDATE t_order_list SET order_status=1,order_pay_time=NOW(),order_final_price = ? WHERE order_number = ? AND order_status=0",
-          [order_final_price/100,tradeNo],
-          function (error, result, fields) {
+          'insert  into t_order_info_list (user_place_order_img,user_place_order_time,order_id,created_time) values (?,Now(),?,NOW())',
+          [JSON.stringify(imagesList), order_id],
+          function (error, resu, fields) {
             if (error) {
-              console.log(tradeNo + '订单更新失败,错误原因：' + error)
+              console.log('新建订单信息记录失败，error:' + error)
             } else {
-              console.log(tradeNo + '订单更新成功')
+              console.log('新建订单信息记录成功，记录ID为：' + resu.insertId)
             }
           }
-        )
-      } else {
-        //失败
-        console.log(
-          '当前订单的支付状态，result_code:' +
-            jsonData.result_code +
-            'return_code:' +
-            jsonData.return_code
         )
       }
     }
   )
+})
 module.exports = router
