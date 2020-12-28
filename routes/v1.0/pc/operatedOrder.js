1150d8b84116cf87228fb69b50227fb4245de885
zhaofelix <15021179915@163.com>
2020-12-28 19:15:48 +0800

    fix:更新

--- routes/v1.0/pc/operatedOrder.js
+++ routes/v1.0/pc/operatedOrder.js
@@ -9,60 +9,24 @@ var url = require('url')
 
 // TODO:根据线路类型查询今日值班的司机
 router.get('/driver/query', function (req, res, next) {
-  let parseObj = url.parse(req.url, true)
-  let query = parseObj.query
-  let router_type = parseInt(query.type)
-  let time = query.time
-  let date = new Date(time)
-  let current_month = date.getMonth() + 1
-  let current_date = date.getDate()
-  console.log(current_month, current_date)
   return new Promise((resolve, reject) => {
-    if (router_type == 4) {
-      DB.queryDB(
-        'SELECT driver_name,car_router_type,router_note,driver_id,driver_is_substitutes from t_driver_list where car_router_type = ? and driver_id not in (SELECT driver_id from t_order_list WHERE order_status = 3 or order_status = 4)',
-        [router_type],
-        function (error, result, fields) {
-          if (error) {
-            reject('查询信息失败，error:' + error)
-          } else {
-            resolve(result)
-          }
-        }
-      )
-    } else {
-      DB.queryDB(
-        'select  driver_name,router_type,driver_schedule,router_note,driver_id,driver_is_substitutes from t_driver_schedule where schedule_month = ? and router_type = ? and driver_id not in (SELECT driver_id from t_order_list WHERE ( order_status not between  3 and 7 )  and driver_id is not null)',
-        [current_month, router_type],
-        function (error, result, fields) {
-          if (error) {
-            reject('查询信息失败，error:' + error)
-          } else {
-            resolve(result)
-          }
-        }
-      )
-    }
-  })
-    .then((data) => {
-      let returnResult = []
-      if (router_type == 4) {
-        returnResult = data
-        console.log(returnResult)
-      } else {
-        for (var i = 0; i < data.length; i++) {
-          let obj = data[i]
-          let schedule = obj.driver_schedule.split('.')
-          if (schedule.indexOf(current_date.toString()) == -1) {
-            returnResult.push(obj)
-          }
+    // TODO:是否需要判断已在运输中的司机
+    DB.queryDB(
+      'SELECT driver_name,car_router_type,router_note,driver_id,driver_is_substitutes from t_driver_list',
+      function (error, result, fields) {
+        if (error) {
+          reject('查询信息失败，error:' + error)
+        } else {
+          resolve(result)
         }
       }
-
+    )
+  })
+    .then((data) => {
       let responseJson = {
         code: 20000,
         message: 'success',
-        data: returnResult
+        data: data
       }
       res.send(responseJson)
     })
@@ -176,13 +140,12 @@ router.get('/order/assign', function (req, res, next) {
   let parseObj = url.parse(req.url, true)
   let query = parseObj.query
   let order_id = parseInt(query.order_id)
-  let car_id = parseInt(query.car_id)
   let driver_id = parseInt(query.driver_id)
   // 将订单的状态更新为3
   console.log('测试')
   DB.queryDB(
-    'update  t_order_list set order_status = 3, car_id = ? , driver_id = ? where order_id = ? and  order_is_deleted = 0 and order_status = 1',
-    [car_id, driver_id, order_id],
+    'update  t_order_list set order_status = 3, driver_id = ? where order_id = ? and  order_is_deleted = 0 and order_status = 1',
+    [driver_id, order_id],
     function (error, result, fields) {
       if (error) {
         let responseJson = {
