2105d560e466dc6c3567706c44fdc9dc4953aa8b
zhaofelix <15021179915@163.com>
2021-01-04 16:38:46 +0800

    fix:增加字段

--- routes/v1.0/public/order.js
+++ routes/v1.0/public/order.js
@@ -2,7 +2,7 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2020-11-17 08:57:51
- * @LastEditTime: 2020-12-24 11:01:21
+ * @LastEditTime: 2021-01-04 16:21:38
  * @FilePath: /server-api/routes/v1.0/public/order.js
  * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
  */
@@ -24,6 +24,7 @@ router.post('/wxpay', function (req, res, next) {
     buildArea,
     imagesList,
     isFirst,
+    isAssign,
     name,
     openId,
     orderNote,
@@ -44,7 +45,7 @@ router.post('/wxpay', function (req, res, next) {
     .order(appId, attach, body, openId, money, notify_url, ip)
     .then((result) => {
       DB.queryDB(
-        'INSERT INTO t_order_list (user_id,order_price,user_reserve_time,order_size,order_user_type,order_number, user_phone,user_address,user_is_first,user_note,order_user_name,order_type,order_created_time) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,NOW())',
+        'INSERT INTO t_order_list (user_id,order_price,user_reserve_time,order_size,order_user_type,order_number, user_phone,user_address,user_is_first,order_is_assign,user_note,order_user_name,order_type,order_created_time) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,NOW())',
         [
           userId,
           orderPrice,
@@ -55,6 +56,7 @@ router.post('/wxpay', function (req, res, next) {
           phoneNumber,
           address + subAddress,
           isFirst,
+          isAssign,
           orderNote,
           name,
           orderType
