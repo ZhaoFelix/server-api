2d93c25e1df338fb8159996382f015a676e413dc
zhaofelix <15021179915@163.com>
2021-01-11 15:27:55 +0800

    fix:按时间排序

--- routes/v1.0/pc/ongoing.js
+++ routes/v1.0/pc/ongoing.js
@@ -2,7 +2,7 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2020-12-10 13:26:52
- * @LastEditTime: 2020-12-23 09:32:22
+ * @LastEditTime: 2021-01-11 15:27:12
  * @FilePath: /server-api/routes/v1.0/pc/ongoing.js
  * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
  */
@@ -14,7 +14,7 @@ var DB = require('../../../config/db')
 router.get('/driver', function (req, res, next) {
   // 仅显示当天订单
   DB.queryDB(
-    "select  * from v_assign_order where date_format(user_reserve_time,'%Y-%m-%d') = date_format(NOW(),'%Y-%m-%d') order by  order_created_time desc",
+    "select  * from v_assign_order where date_format(user_reserve_time,'%Y-%m-%d') = date_format(NOW(),'%Y-%m-%d') order by  order_created_time asc",
     function (error, result, fields) {
       if (error) {
         let responseJson = {
