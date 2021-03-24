c2da6931d4acf37ca5f3ce061baa0335a66ae405
zhaofelix <15021179915@163.com>
2021-03-24 22:48:29 +0800

    fix:MySQL改用连接池进行连接

--- config/db.js
+++ config/db.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Felix
+ * @Email: felix@qingmaoedu.com
+ * @Date: 2020-11-03 08:34:04
+ * @LastEditTime: 2021-03-24 22:45:37
+ * @FilePath: /server-api/config/db.js
+ * Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
+ */
 var mysql = require('mysql')
 let config = require('./env')
 // 创建一个连接池
@@ -6,22 +14,32 @@ let pool = mysql.createPool(
 )
 
 //数据库查询，查询时的默认参数为空
+// function queryDB(sql, params = '1', callback) {
+//   pool.getConnection(function (err, connection) {
+//     if (err) {
+//       console.log('连接失败' + err)
+//     } else {
+//       connection.query(sql, params, function (err, results, fields) {
+//         if (err) {
+//           connection.release() //释放连接
+//           console.log('查询失败' + err)
+//           // throw err
+//         }
+//         //查询结束后关闭数据库连接
+
+//         //将查询出来的结果返回给回调函数
+//         callback(err, results, fields)
+//       })
+//     }
+//   })
+// }
+
 function queryDB(sql, params = '1', callback) {
-  pool.getConnection(function (err, connection) {
+  pool.query(sql, params, function (err, results, fields) {
     if (err) {
-      console.log('连接失败' + err)
-    } else {
-      connection.query(sql, params, function (err, results, fields) {
-        if (err) {
-          console.log('查询失败' + err)
-          // throw err
-        }
-        //查询结束后关闭数据库连接
-        connection.release() //释放连接
-        //将查询出来的结果返回给回调函数
-        callback(err, results, fields)
-      })
+      console.log('查询失败' + err)
     }
+    callback(err, results, fields)
   })
 }
 
