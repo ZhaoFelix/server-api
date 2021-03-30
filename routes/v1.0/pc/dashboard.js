d64206231901551640266ddb78a7b311bf4712fd
zhaofelix <15021179915@163.com>
2021-03-30 10:35:57 +0800

    feat: 增加用户增长查询接口

--- routes/v1.0/pc/dashboard.js
+++ routes/v1.0/pc/dashboard.js
@@ -2,7 +2,7 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2020-12-18 14:43:30
- * @LastEditTime: 2021-03-18 08:55:59
+ * @LastEditTime: 2021-03-30 10:35:24
  * @FilePath: /server-api/routes/v1.0/pc/dashboard.js
  * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
  */
@@ -33,6 +33,19 @@ router.get('/week', function (req, res, next) {
         new Result(error, '查询订单失败').fail(res)
       } else {
         new Result(result, 'success').success(res)
+      }
+    }
+  )
+})
+// 查询最近7天的用户增长情况
+router.get('/week/user', function (req, res, next) {
+  DB.queryDB(
+    "select DATE_FORMAT(wechat_created_time,'%Y-%m-%d') days,count(user_id) count from t_user_list  where DATE_SUB(curdate(),INTERVAL 7 DAY) <= date(wechat_created_time) group by days",
+    function (error, result, fields) {
+      if (error) {
+        new Result(error, '查询用户失败').fail(res)
+      } else {
+        new Result(result, 'sucess').success(res)
       }
     }
   )
