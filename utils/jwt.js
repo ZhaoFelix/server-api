ab7ee772159542e108ac15c40bda15a434ae2ca6
zhaofelix <15021179915@163.com>
2021-04-22 16:12:44 +0800

    fix:支付回调取消jwt验证

--- utils/jwt.js
+++ utils/jwt.js
@@ -2,7 +2,7 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2021-03-17 13:06:21
- * @LastEditTime: 2021-04-06 10:38:54
+ * @LastEditTime: 2021-04-22 16:12:30
  * @FilePath: /server-api/utils/jwt.js
  * Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
  */
@@ -15,5 +15,5 @@ module.exports = jwt({
   algorithms: ['HS256']
 }).unless({
   // 不进行安全认证的白名单
-  path: ['/', '/v1.0/pc/login/login']
+  path: ['/', '/v1.0/pc/login/login', '/v1.0/public/order/callback']
 })
