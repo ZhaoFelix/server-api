344f85ce210925dc471277a8a5c2006d664cb8f9
zhaofelix <15021179915@163.com>
2021-05-13 10:49:10 +0800

    fix:增加销售额接口

--- routes/v1.0/pc/dashboard.js
+++ routes/v1.0/pc/dashboard.js
@@ -2,7 +2,7 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2020-12-18 14:43:30
- * @LastEditTime: 2021-05-12 20:25:45
+ * @LastEditTime: 2021-05-13 10:19:53
  * @FilePath: /server-api/routes/v1.0/pc/dashboard.js
  * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
  */
@@ -30,7 +30,6 @@ router.get('/week', function (req, res, next) {
   let parseObj = url.parse(req.url, true)
   let query = parseObj.query
   let type = query.type
-
   let sql =
     type == 'order'
       ? "select DATE_FORMAT(order_created_time,'%Y-%m-%d') days,count(order_id) count from t_order_list where order_status != 2 and DATE_SUB(curdate(),INTERVAL 7 DAY) <= date(order_created_time) group by days"
@@ -43,7 +42,24 @@ router.get('/week', function (req, res, next) {
     }
   })
 })
-
+// 销售额统计
+router.get('/sale', function (req, res, next) {
+  let parseObj = url.parse(req.url, true)
+  let query = parseObj.query
+  let timeGap = query.timeGap
+  let sql = `select DATE_FORMAT(order_created_time,'%Y-%m-%d') days,round(sum(order_final_price),2) count from t_order_list where order_status != 2 and DATE_SUB(curdate(),INTERVAL ? DAY) <= date(order_created_time) group by days;`
+  DB.queryDB(
+    sql,
+    timeGap == undefined ? 7 : timeGap,
+    function (error, result, fields) {
+      if (error) {
+        new Result(error, '查询订单失败').fail(res)
+      } else {
+        new Result(result, 'success').success(res)
+      }
+    }
+  )
+})
 // 查询已认证和未认证的司机(物业)比例
 router.get('/driver/auth', function (req, res, next) {
   let parseObj = url.parse(req.url, true)
@@ -74,4 +90,5 @@ router.get('/order/ratio', function (req, res, next) {
     }
   })
 })
+
 module.exports = router
