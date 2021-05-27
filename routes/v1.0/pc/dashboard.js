aae91628ce992401fe82836ea44e724881a2275b
zhaofelix <15021179915@163.com>
2021-05-27 15:55:15 +0800

    fix:增加今日图表查询接口

--- routes/v1.0/pc/dashboard.js
+++ routes/v1.0/pc/dashboard.js
@@ -2,7 +2,7 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2020-12-18 14:43:30
- * @LastEditTime: 2021-05-25 08:26:47
+ * @LastEditTime: 2021-05-27 15:40:54
  * @FilePath: /server-api/routes/v1.0/pc/dashboard.js
  * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
  */
@@ -79,11 +79,21 @@ router.get('/driver/auth', function (req, res, next) {
 })
 
 router.get('/order/ratio', function (req, res, next) {
-  let sql = `select count(distinct case when order_type = 1 then order_id else null end) as '居民装修',
+  let parseObj = url.parse(req.url, true)
+  let query = parseObj.query
+  let type = query.type
+  let sql =
+    type == undefined
+      ? `select count(distinct case when order_type = 1 then order_id else null end) as '居民装修',
   count(distinct case when order_type = 2 then order_id else null end) as '商业装修',
   count(distinct case when order_type = 3 then order_id else null end) as '垃圾箱清运',
   count(distinct case when order_type = 11 then order_id else null end) as '二次清运'
   from t_order_list where order_status != 2;`
+      : `select count(distinct case when order_type = 1 then order_id else null end) as '居民装修',
+      count(distinct case when order_type = 2 then order_id else null end) as '商业装修',
+      count(distinct case when order_type = 3 then order_id else null end) as '垃圾箱清运',
+      count(distinct case when order_type = 11 then order_id else null end) as '二次清运'
+      from t_order_list where order_status != 2 and  DATE_FORMAT(order_created_time,'%Y-%m-%d') = DATE_FORMAT(now(),'%Y-%m-%d');`
   DB.queryDB(sql, function (error, result, fields) {
     if (error) {
       new Result(error, '查询失败').fail(res)
@@ -94,10 +104,19 @@ router.get('/order/ratio', function (req, res, next) {
 })
 
 router.get('/status/ratio', function (req, res, next) {
-  let sql = `select count(distinct case when order_status = 2 then order_id else null end) as '被取消订单',
+  let parseObj = url.parse(req.url, true)
+  let query = parseObj.query
+  let type = query.type
+  let sql =
+    type == undefined
+      ? `select count(distinct case when order_status = 2 then order_id else null end) as '被取消订单',
   count(distinct case when order_status = 6 then order_id else null end) as '已完成订单',
   count(distinct case when order_status != 2 and order_status != 6 and order_status != 0 then order_id else null end) as '进行中订单',count(distinct case when order_status = 0 then order_id else null end) as '待支付订单'
   from t_order_list;`
+      : `select count(distinct case when order_status = 2 then order_id else null end) as '被取消订单',
+      count(distinct case when order_status = 6 then order_id else null end) as '已完成订单',
+      count(distinct case when order_status != 2 and order_status != 6 and order_status != 0 then order_id else null end) as '进行中订单',count(distinct case when order_status = 0 then order_id else null end) as '待支付订单'
+      from t_order_list where DATE_FORMAT(order_created_time,'%Y-%m-%d') = DATE_FORMAT(now(),'%Y-%m-%d');`
   DB.queryDB(sql, function (error, result, fields) {
     if (error) {
       new Result(error, '查询失败').fail(res)
